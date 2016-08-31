02954ccef0aa2f4084977ab165f52cc073587e43
Ulisses Albuquerque <ulisses.albuquerque@team.telstra.com>
2016-08-31 15:39:28 +1000

    First security issue added

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const request = require('request');
 const xmldom = require('xmldom').DOMParser;
 const xpath = require('xpath');
 
+function display(string) {
+  /* This should be flagged */
+  eval(`logger.info("${string}")`);
+}
+
 logger.info('Fetching RSS from YCombinator');
 request.get('https://news.ycombinator.com/rss', function(error, response, body) {
   const xmldoc = new xmldom().parseFromString(body);
@@ -16,6 +21,6 @@ request.get('https://news.ycombinator.com/rss', function(error, response, body) 
   _(items).forEach(function(item) {
     const title = xpath.select('title/text()', item);
     const link = xpath.select('link/text()', item)
-    logger.info(`${title} -- ${link}`);
+    display(`${title} -- ${link}`);
   });
 });
